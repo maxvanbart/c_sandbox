bd58b41ae976ee0172e1e5c8b441518d6e279de1
Max van Bart <maxvanbart@gmail.com>
2023-02-23 00:35:45 +0100

    recursive fibonacci sequence

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+int fibonacci(int n) {
+    if (n <= 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+
+
 int main() {
     printf("Hello World!\n");
+
+    int i = fibonacci(8);
+    const char out = (char) i;
+
+    printf("The fibonacci number is: %d\n", i);
     return 0;
 }
